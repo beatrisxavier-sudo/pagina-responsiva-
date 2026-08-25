de56263f73693a69f765ae4cf8d0e1a45e9b3c1b
beatrisxavier-sudo <beatris.xavier@escola.pr.gov.br>
2026-08-25 10:23:07 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.post-btn');
+
+  button.addEventListener('click', () => {
+    alert('Você clicou na postagem horizontal!');
+  });
+});
